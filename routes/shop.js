2ce277b26b675c48dca935a08d3b6f8d0feacbe8
Aaron24000 <aaron.nobles@outlook.com>
2021-04-12 22:18:14 -0400

    changed my files to stream instead of download

--- routes/shop.js
+++ routes/shop.js
@@ -15,6 +15,7 @@ const {
   postCart,
   postCartDeleteProduct,
   postOrder,
+  getInvoice
 } = require("../controllers/shop");
 
 const router = express.Router();
@@ -34,5 +35,7 @@ router.post("/cart-delete-item", isAuth, postCartDeleteProduct);
 router.post('/create-order', isAuth, postOrder);
 
 router.get("/orders", isAuth, getOrders);
+
+router.get('/orders/:orderId', isAuth, getInvoice);
 
 module.exports = router;
